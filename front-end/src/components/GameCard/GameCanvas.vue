c94d2145148e0c97ca1b60a1fca40b86eb9b1fe1
Veeru <immaculateempyrean@gmail.com>
2021-09-15 05:58:24 +0530

    successfully drawing the background in game canvas.

--- front-end/src/components/GameCard/GameCanvas.vue
+++ front-end/src/components/GameCard/GameCanvas.vue
@@ -5,8 +5,37 @@
 </template>
 
 <script>
+import { Stage, Shape } from "@createjs/easeljs";
+
 export default {
 	name: "GameCanvas",
+	methods: {
+		loadBackground(stage) {
+			let background = new Shape();
+			background.graphics
+				.beginLinearGradientFill(
+					["#2573BB", "#6CB8DA", "#567A32"],
+					[0.0, 0.27, 0.3],
+					0,
+					0,
+					0,
+					480
+				)
+				.drawRect(0, 0, 320, 480);
+
+			background.x = 0;
+			background.y = 0;
+			background.name = "background";
+			background.cache(0, 0, 320, 480);
+
+			stage.addChild(background);
+			stage.update();
+		},
+	},
+	mounted() {
+		let stage = new Stage("gameCanvas");
+		this.loadBackground(stage);
+	},
 };
 </script>
 
